50b65f67acc20f75e8b6d052258cfac733f59f01
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-10-08 10:37:45 +0100

    Create test folder and one test

---
+++ test/connectors/CustomsReferenceDataConnectorSpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import java.io.File
+
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, post, urlEqualTo}
+import org.scalatest.OptionValues
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.ws.WSResponse
+import uk.gov.hmrc.customsreferencedatatestfrontend.connectors.CustomsReferenceDataConnector
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+class CustomsReferenceDataConnectorSpec
+  extends AnyFreeSpec
+    with Matchers
+    with ScalaFutures
+    with IntegrationPatience
+    with OptionValues
+    with WiremockSuite {
+
+  override protected def portConfigKey: String = "microservice.services.customs-reference-data.port"
+
+  lazy val connector: CustomsReferenceDataConnector = app.injector.instanceOf[CustomsReferenceDataConnector]
+
+  implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
+
+  "CustomsReferenceDataConnector" - {
+
+    "referenceDataListpost" - {
+
+      "must return status Accepted" in {
+
+        server.stubFor(
+          post(urlEqualTo("/customs-reference-data/reference-data-lists"))
+            .willReturn(
+              aResponse()
+                .withStatus(202)
+            )
+        )
+
+        val tempFile = File.createTempFile("test", ".gz")
+
+        val result: Future[WSResponse] = connector.referenceDataListpost(tempFile)
+
+        result.futureValue.status mustBe 202
+
+        tempFile.deleteOnExit()
+      }
+    }
+  }
+
+}
---
+++ test/connectors/WiremockSuite.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
+import play.api.Application
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceableModule}
+
+trait WiremockSuite extends BeforeAndAfterAll with BeforeAndAfterEach {
+  this: Suite =>
+
+  protected val server: WireMockServer = new WireMockServer(wireMockConfig().dynamicPort())
+
+  protected def portConfigKey: String
+
+  protected lazy val app: Application =
+    new GuiceApplicationBuilder()
+      .configure(
+        portConfigKey -> server.port().toString
+      )
+      .overrides(bindings: _*)
+      .build()
+
+  protected def bindings: Seq[GuiceableModule] = Seq.empty
+
+  override def beforeAll(): Unit = {
+    server.start()
+    app
+    super.beforeAll()
+  }
+
+  override def beforeEach(): Unit = {
+    server.resetAll()
+    app
+    super.beforeEach()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    server.stop()
+  }
+
+}
