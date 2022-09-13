5f68f980073bf34f47cd546926d6fa818511392e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-09-13 14:31:32 +0100

    CTCP-1200: Updated bootstrap version

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
 
 val appName = "customs-reference-data-test-frontend"
 
-val silencerVersion = "1.7.1"
+val silencerVersion = "1.7.9"
 
 lazy val microservice = Project(appName, file("."))
   .enablePlugins(
@@ -15,15 +15,9 @@ lazy val microservice = Project(appName, file("."))
   .disablePlugins(JUnitXmlReportPlugin)
   .settings(
     majorVersion := 0,
-    scalaVersion := "2.12.11",
+    scalaVersion := "2.12.15",
     libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test,
     PlayKeys.playDefaultPort := 9493,
-    TwirlKeys.templateImports ++= Seq(
-      "uk.gov.hmrc.customsreferencedatatestfrontend.config.AppConfig",
-      "uk.gov.hmrc.govukfrontend.views.html.components._",
-      "uk.gov.hmrc.govukfrontend.views.html.helpers._",
-      "uk.gov.hmrc.hmrcfrontend.views.html.components._"
-    ),
     // ***************
     // Use the silencer plugin to suppress warnings
     // You may turn it on for `views` too to suppress warnings from unused imports in compiled twirl templates, but this will hide other warnings.
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,21 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapVersion = "7.3.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.14.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.14.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-govuk"        % "2.0.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.12.0",
-    "org.scalatest"           %% "scalatest"                % "3.1.2",
-    "org.jsoup"               %  "jsoup"                    % "1.10.2",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion,
+    "org.scalatest"           %% "scalatest"                % "3.2.12",
     "com.typesafe.play"       %% "play-test"                % current,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3",
-    "org.scalatestplus"       %% "scalatestplus-mockito"    % "1.0.0-M2",
-    "com.github.tomakehurst"  % "wiremock-standalone"       % "2.27.2",
-    "org.scalacheck"          %% "scalacheck"               % "1.14.3"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0",
+    "org.mockito"             %  "mockito-core"             % "4.8.0",
+    "org.scalatestplus"       %% "mockito-4-5"              % "3.2.12.0",
+    "org.jsoup"               %  "jsoup"                    % "1.15.3",
+    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"
   ).map(_ % Test)
 }
