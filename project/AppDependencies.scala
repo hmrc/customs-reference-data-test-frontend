bb559053f018263d25132d96d8df5f9d6bfb4c91
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 14:55:40 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion
@@ -11,10 +11,10 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatest"          %% "scalatest"              % "3.2.19",
-    "org.mockito"            %  "mockito-core"           % "5.14.2",
+    "org.mockito"            %  "mockito-core"           % "5.18.0",
     "org.scalatestplus"      %% "mockito-5-12"           % "3.2.19.0",
-    "org.scalacheck"         %% "scalacheck"             % "1.18.0",
+    "org.scalacheck"         %% "scalacheck"             % "1.18.1",
     "org.scalatestplus"      %% "scalacheck-1-18"        % "3.2.19.0",
-    "org.jsoup"              %  "jsoup"                  % "1.18.1"
+    "org.jsoup"              %  "jsoup"                  % "1.21.1"
   ).map(_ % Test)
 }
